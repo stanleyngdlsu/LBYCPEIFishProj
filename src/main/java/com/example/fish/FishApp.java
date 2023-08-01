bba3270673a14e858f81c2bf1b871c9696312817
jacob-go-11 <124241493+jacob-go-11@users.noreply.github.com>
2023-08-01 19:33:42 +0800

    Add files via upload

---
+++ src/main/java/com/example/fish/FishApp.java
@@ -0,0 +1,56 @@
+package com.example.fish;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import javafx.animation.FadeTransition;
+
+public class FishApp extends Application {
+
+    private static Stage window;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        window = stage;
+        window.setResizable(false);
+        window.initStyle(StageStyle.UNDECORATED);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        scene.getStylesheets().add("design.css");
+        window.setScene(scene);
+        window.show();
+    }
+
+    public FXMLLoader changeScene(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxml));
+        Parent root = fxmlLoader.load();
+
+        Scene currentScene = window.getScene();
+        Scene newScene = new Scene(root, 600, 400);
+        newScene.getStylesheets().add("design.css");
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(500), currentScene.getRoot());
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+        fadeOut.setOnFinished(e -> {
+            window.setScene(newScene);
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(500), newScene.getRoot());
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.play();
+        });
+        fadeOut.play();
+        return fxmlLoader;
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
